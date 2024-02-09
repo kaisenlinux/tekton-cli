da747ebd2f2884410acbd30265009611995fe5a5
Kevin Chevreuil <kaisen@kaisenlinux.org>
2024-02-09 18:15:02 +0100

    Initial commit

---
+++ choco/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop'; 
+$toolsDir    = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$packageName = 'tektoncd-cli'
+$url64       = 'https://github.com/tektoncd/cli/releases/download/v0.34.0/tkn_0.34.0_Windows_x86_64.zip'
+
+$packageArgs = @{
+  packageName    = $packageName
+  unzipLocation  = $toolsDir
+  url64bit       = $url64
+  softwareName   = 'tektoncd-cli*' 
+  checksum64     = 'b87ceebeea935522faab40d420d5893d52de01437fb386eeab9556167e6b20fe'
+  checksumType64 = 'sha256'
+}
+
+Install-ChocolateyZipPackage @packageArgs
